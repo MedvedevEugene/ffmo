50d0234ae7c9debc60a11acbb9d9a642e37697e5
MedvedevEugene <medvedev_ea67@mail.ru>
2026-03-11 21:06:53 +0300

    Удалена неиспользуемая функция exportJSON

--- src/App.tsx
+++ src/App.tsx
@@ -116,17 +116,6 @@ function App() {
     return labels[type] || type;
   };
 
-  const exportJSON = () => {
-    const dataStr = JSON.stringify(match, null, 2);
-    const dataBlob = new Blob([dataStr], { type: 'application/json' });
-    const url = URL.createObjectURL(dataBlob);
-    const link = document.createElement('a');
-    link.href = url;
-    link.download = `protocol-${match.id}.json`;
-    link.click();
-    URL.revokeObjectURL(url);
-  };
-
   return (
     <div className="container">
       <div className="header">
